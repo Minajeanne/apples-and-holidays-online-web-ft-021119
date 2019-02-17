4eaa7890578dcff85081cd6314d5b06725c99b26
Alicia Santiago <albrown07@mac.com>
2019-02-17 20:02:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, item|
+      if holiday.
     
     
   # return an array of holiday names (as symbols) where supply lists
