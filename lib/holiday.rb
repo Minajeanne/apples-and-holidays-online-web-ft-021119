95f7749f2ddcc03c4259fbe436f3821cdd49ed10
Alicia Santiago <albrown07@mac.com>
2019-02-17 20:09:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,9 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, item|
       holiday if item.include?("BBQ")
-      holiday.flatten
     end
-  end
+  end.flatten
 end
     
     
